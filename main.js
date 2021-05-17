23c977720b050959c0c3ec787c7af563aac2a015
gimyuji476@gmail.com <gimyuji476@gmail.com>
2021-05-18 02:55:01 +0900

    카테고리 함수 비동기처리문제 발생

--- main.js
+++ main.js
@@ -12,33 +12,53 @@ const db = mysql.createConnection({
   password: '940428',
   database: 'opentutorials',
 });
-let topiclist = [];
 
 db.connect();
 
-db.query('SELECT * FROM topic', function (error, topics) {
-  if (error) {
-    console.log(error);
-  }
-  for (i = 0; i < topics.length; i++) {
-    let topic = {
-      id: topics[i].id,
-      title: topics[i].title,
-    };
+// db.query('SELECT * FROM topic', function (error, topics) {
+//   if (error) {
+//     console.log(error);
+//   }
+//   for (i = 0; i < topics.length; i++) {
+//     let topic = {
+//       id: topics[i].id,
+//       title: topics[i].title,
+//     };
 
-    topiclist.push(topic);
-  }
-});
+//     topiclist.push(topic);
+//   }
+// });
 
 const app = http.createServer((request, response) => {
   const _url = request.url;
   const myURL = new URL('http://localhost:3000' + _url);
   const queryData = myURL.searchParams.get('id');
   const pathname = myURL.pathname;
+  let test = [];
+  function topiclist(callbackFunc) {
+    db.query('SELECT * FROM topic', function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      callbackFunc(topics);
+    });
+  }
 
   if (pathname === '/') {
     if (queryData === null) {
-      const list = template.list(topiclist);
+      //콜백함수를 통해 template.list 함수 파라미터로 topiclist 함수 리턴값 전달까지 성공했으나 변수에 넣으면 undefined 오류 발생
+      topiclist((list) => {
+        let testlist = template.list(list);
+        return testlist;
+      });
+
+      // console.log(
+      //   template.list(
+      //     topiclist((list) => {
+      //       return list;
+      //     })
+      //   )
+      // );
       const title = 'Welcome!';
       const description = 'Hello, nodejs!';
       const html = template.HTML(
@@ -53,17 +73,15 @@ const app = http.createServer((request, response) => {
       db.query(
         `SELECT * FROM topic WHERE id=?`,
         [queryData],
-        (error, topics) => {
+        (error, topic) => {
           if (error) {
             throw error;
           }
-          const title = topics[0].title;
-          const description = topics[0].description;
           const list = template.list(topiclist);
           const html = template.HTML(
-            title,
+            topic[0].title,
             list,
-            `<h2>${title}</h2><p>${description}</p>`,
+            `<h2>${topic[0].title}</h2><p>${topic[0].description}</p>`,
             `<a href="/create">create<a>
             <a href="/update?id=${queryData}">update<a>
             <form action="delete_process" method="post">
@@ -77,13 +95,12 @@ const app = http.createServer((request, response) => {
       );
     }
   } else if (pathname === '/create') {
-    fs.readdir('./data', (err, datalist) => {
-      const title = 'WEB - create';
-      const list = template.list(datalist);
-      const html = template.HTML(
-        title,
-        list,
-        `
+    const title = 'Create';
+    const list = template.list(topiclist);
+    const html = template.HTML(
+      title,
+      list,
+      `
         <form action="/create_process" method="post">
         <p>
         <input type="text" name="title" placeholder="title">
@@ -96,11 +113,10 @@ const app = http.createServer((request, response) => {
         </p>
         </form>
         `,
-        ''
-      );
-      response.writeHead(200);
-      response.end(html);
-    });
+      ''
+    );
+    response.writeHead(200);
+    response.end(html);
   } else if (pathname === '/create_process') {
     let body = '';
     request.on('data', (data) => {
@@ -108,78 +124,67 @@ const app = http.createServer((request, response) => {
     });
     request.on('end', () => {
       //인코딩된 쿼리스트링값 객체화
-      console.log(body);
       const post = qs.parse(body);
-      console.log(post);
-      const title = post.title;
-      const description = post.description;
-      connection.query(
-        `INSERT INTO topic(title, description) VALUES(${title}, ${description})`,
-        function (error, results, fields) {
+
+      db.query(
+        `INSERT INTO topic(title, description, created, author_id) 
+        VALUES(?, ?, NOW(), ?)`,
+        [post.title, post.description, 1],
+        function (error, result) {
           if (error) {
-            console.log(error);
+            throw error;
           }
           response.writeHead(302, {
-            Location: `/?id=${qs.escape(title)}`,
+            Location: `/?id=${qs.escape(result.insertId)}`,
           });
           response.end();
         }
       );
     });
   } else if (pathname === '/update') {
-    fs.readdir('./data', (err, datalist) => {
-      const filteredId = path.parse(queryData).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-        //update시 쿼리스트링 id값에 존재하지 않는 data 삽입 -> 404
-        if (description === undefined) {
-          response.writeHead(404);
-          response.end('Not Found');
-          return;
-        }
-        const title = queryData;
-        const list = template.list(datalist);
-        const html = template.HTML(
-          title,
-          list,
-          `
+    db.query(`SELECT * FROM topic WHERE id=?`, [queryData], (error, topic) => {
+      if (error) {
+        throw error;
+      }
+      const list = template.list(topiclist);
+      const html = template.HTML(
+        topic[0].title,
+        list,
+        `
         <form action="/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value=${title}>
+        <input type="hidden" name="id" value="${topic[0].id}">
+        <p><input type="text" name="title" placeholder="title" value=${topic[0].title}>
         </p>
         <p>
-        <textarea name="description" placeholder="description">${description}</textarea>
+        <textarea name="description" placeholder="description">${topic[0].description}</textarea>
         </p>
         <p>
         <input type="submit">
         </p>
         </form>
         `,
-          `<a href="/create">create<a> <a href="/update?id=${title}">update<a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+        `<a href="/create">create<a> <a href="/update?id=${topic[0].id}">update<a>`
+      );
+      response.writeHead(200);
+      response.end(html);
     });
-  } else if (pathname === '/create_process') {
+  } else if (pathname === '/update_process') {
     let body = '';
     request.on('data', (data) => {
       body = body + data;
     });
     request.on('end', () => {
       //인코딩된 쿼리스트링값 객체화
-      console.log(body);
       const post = qs.parse(body);
-      console.log(post);
-      const title = post.title;
-      const description = post.description;
-      connection.query(
-        `INSERT INTO topic(title, description) VALUES(${title}, ${description})`,
-        function (error, results, fields) {
+      db.query(
+        `UPDATE topic SET title = ?, description = ? WHERE id = ${post.id}`,
+        [post.title, post.description],
+        function (error) {
           if (error) {
-            console.log(error);
+            throw error;
           }
           response.writeHead(302, {
-            Location: `/?id=${qs.escape(title)}`,
+            Location: `/?id=${qs.escape(post.id)}`,
           });
           response.end();
         }
@@ -192,14 +197,19 @@ const app = http.createServer((request, response) => {
     });
     request.on('end', () => {
       const post = qs.parse(body);
-      const id = post.id;
-      const filteredId = path.parse(id).base;
-      fs.unlink(`./data/${filteredId}`, (err) => {
-        response.writeHead(302, {
-          Location: '/',
-        });
-        response.end();
-      });
+      db.query(
+        'DELETE FROM topic WHERE id = ?',
+        [post.id],
+        function (error, result) {
+          if (error) {
+            throw error;
+          }
+          response.writeHead(302, {
+            Location: '/',
+          });
+          response.end();
+        }
+      );
     });
   } else {
     response.writeHead(404);
